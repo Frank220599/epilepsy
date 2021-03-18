f19ac743490bf6b07b1c085416211c0093396432
uzcoding <millioner.sm.8@gmail.com>
2021-03-18 14:45:41 +0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Epilepsy());
+}
+
+class Epilepsy extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
